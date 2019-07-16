3e6ea8b382299eefae4723caf73fec03979d5365
Ayesiza Hawah <yesiha2001@gmail.com>
2019-07-15 20:14:23 -0700

    chore(eslint-fix):eslint-format-and-fix [finishes#167310036]

--- index.js
+++ index.js
@@ -1,35 +1,34 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import bodyParser from 'body-parser';
 import apiRouters from './app/routers/apiRouters'
-import swaggerUi from 'swagger-ui-express'
-import swaggerDoc from './swagger.json'
-import bodyParser from 'body-parser'
+import swaggerDoc from './swagger.json';
 
-// app.use(bodyParser.json());
+app.use(bodyParser.json());
 
-// app.use(bodyParser.urlencoded({ extended: false }));
- 
+app.use(bodyParser.urlencoded({ extended: false }));
 
 
-const app = express()
+const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
 
-app.use('/api/v1/', apiRouters)
-app.use('/doc',swaggerUi.serve,swaggerUi.setup(swaggerDoc));
+app.use('/api/v1/', apiRouters);
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 // where page not found
 app.use((req, res, next) => {
-    const error = new Error('method used is not allowed');
-    error.status = 405;
-    next(error);
-  });
-  
-  app.use((error, req, res, next) => {
-    res.status(error.status || 500).send({ error: error.status || 500, message: error.message });
-    next();
-  });
+  const error = new Error('method used is not allowed');
+  error.status = 405;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500).send({ error: error.status || 500, message: error.message });
+  next();
+});
 
 
 app.listen(port, () => console.log(`listening on port ${port}...!`));
